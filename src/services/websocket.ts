8f726e60a19f8ffbf52e0bb89c55d195f8312587
Nitin Goyal <nitingoyal3150@gmail.com>
2025-11-02 19:12:45 -0500

    changed the production url

--- src/services/websocket.ts
+++ src/services/websocket.ts
@@ -188,7 +188,7 @@ class WebSocketService {
 // Create and export the instance
 const wsUrl = import.meta.env.DEV
   ? "ws://localhost:8000/admin/ws"
-  : "wss://smartbag-backend-oqlt.onrender.com/admin/ws";
+  : "ws://195.35.6.222/admin-api/ws";
 
 export const wsService = new WebSocketService(wsUrl);
 
